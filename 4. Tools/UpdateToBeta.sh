d571e0398439a88be6d564a7532fb3d134399d49
Ramalama2 <rama@ramas-air.home>
2014-02-21 20:17:55 +0100

    Script for AppStore (BetaUpdates)

---
+++ 4. Tools/UpdateToBeta.sh
@@ -0,0 +1,1 @@
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate CatalogURL https://swscan.apple.com/content/catalogs/others/index-10.9seed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
