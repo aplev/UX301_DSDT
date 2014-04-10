f60884f4bddef62ad0e7adcb0301ae54366ecb23
Ramalama2 <rama@ramas-macbook-air.fritz.box>
2014-04-10 17:09:47 +0200

    Update to Beta

--- 5. Tools/UpdateToBeta.sh
+++ 5. Tools/UpdateToBeta.sh
@@ -1,1 +1,1 @@
-sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate CatalogURL https://swscan.apple.com/content/catalogs/others/index-10.9seed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
+sudo /usr/sbin/softwareupdate --set-catalog "https://swscan.apple.com/content/catalogs/others/index-10.9seed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
